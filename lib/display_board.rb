4a29da9f2e8442b58d92e3aff15c8a4ee606deb6
Dan Drumwright <danieldrumwright@gmail.com>
2018-01-17 20:12:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,54 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board( board = [" "," "," "," "," "," "," "," "," "])
+  
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+end 
+
+
+def display_board( board = [" "," "," "," "," "," "," "," "," "])
+ 
+puts " O |   |   "
+
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+end 
+
+
+def display_board( board = [" "," "," "," "," "," "," "," "," "])
+ 
+puts " X | X | X "
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+end 
+ 
+ 
+ def display_board( board = [" "," "," "," "," "," "," "," "," "])
+ 
+display_board( board = [" "," "," "," "," "," "," "," "," "]) 
+puts "place X #{[4]}"
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+end 
+ 
+    
+   
